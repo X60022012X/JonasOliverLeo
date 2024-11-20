4f1bdba5b971432bda43e4468d2432e6c1d857e7
X60022012X <leo.fredenborg@gmail.com>
2024-11-20 08:47:03 +0100

    wet -> dry

--- direct_api.py
+++ direct_api.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 def get_suggestion(city):
     api_key = "ba9b0f3ce4e121bee6ffb531794ec625"
@@ -9,23 +8,14 @@ def get_suggestion(city):
     response_direct = requests.get(url_direct)
     data_direct = response_direct.json()
 
-    suggestions = []
-    for i in data_direct:
-        temp_dict = {}
+    suggestions = {}
 
-        if i.get("state"):
-            temp_dict["place"] = f"{i['name']}, {i['state']}, {i['country']}"
-        else:
-            temp_dict["place"] = f"{i['name']}, {i['country']}"
+    for location in data_direct:
 
-        temp_dict["coordinates"] = [i['lat'], i['lon']]
+        place = f"{location['name']}, {location.get('state', '')}, {location['country']}".replace(", ,", ",")
+        coords = [location['lat'], location['lon']]
         
-        suggestions.append(temp_dict)
+        if place not in suggestions:
+            suggestions[place] = coords
 
-    # Print all collected suggestions
-    pprint(suggestions)
-
-    # Optionally, print the raw response data
-    pprint(data_direct)
-
-get_suggestion("Oslo")
+    return suggestions
