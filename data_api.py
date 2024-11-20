2731a7a5e23f392d35349a5642f2649b04c595e7
X60022012X <leo.fredenborg@gmail.com>
2024-11-20 22:32:56 +0100

    Wet -> dry

--- data_api.py
+++ data_api.py
@@ -1,9 +1,5 @@
 import requests
-from pprint import pprint
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 from datetime import datetime
-
 
 
 def get_data(lat, lon):
@@ -20,50 +16,46 @@ def get_data(lat, lon):
 
 
 
-def render_info(data_forkast):
-    weather_info = {}
+
+
+def return_city_info(lat, lon):
+    forkast_data = get_data(lat, lon)
+    weather_info = forkast_data.get("city", {}).copy()
+
+
+    weather_info["entries"] = forkast_data.get("cnt")
+    weather_info["code"] = forkast_data.get("cod")
+
+
+    def format_unix_time(unix_time):
+        if unix_time is not None:
+            return {"Unix": unix_time, "ISO_8601": datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')}
+        else:
+            return {"Unix": None, "ISO_8601": None}
     
-    for x, y in data_forkast.get("city", {}).items():
-        weather_info[x] = y
-
-    weather_info["entries"] = data_forkast.get("cnt", None)
-    weather_info["code"] = data_forkast.get("cod", None)
-    sunrise_unix = weather_info.get("sunrise", None)
-    sunset_unix = weather_info.get("sunset", None)
-    
-    if sunrise_unix is not None:
-        weather_info["sunrise"] = {
-            "Unix": sunrise_unix,
-            "UTC": datetime.utcfromtimestamp(sunrise_unix).strftime('%Y-%m-%d %H:%M:%S')
-        }
-    else:
-        weather_info["sunrise"] = {"Unix": None, "UTC": None}
-
-    if sunset_unix is not None:
-        weather_info["sunset"] = {
-            "Unix": sunset_unix,
-            "UTC": datetime.utcfromtimestamp(sunset_unix).strftime('%Y-%m-%d %H:%M:%S')
-        }
-    else:
-        weather_info["sunset"] = {"Unix": None, "UTC": None}
-
+    weather_info["sunrise"] = format_unix_time(weather_info.get("sunrise"))
+    weather_info["sunset"] = format_unix_time(weather_info.get("sunset"))
   
-    timezone_sec = weather_info.get("timezone", None)
+
+    timezone_sec = weather_info.get("timezone")
     if timezone_sec is not None:
-      z = "" 
-      if timezone_sec > 0:
-        z = "+"
         weather_info["timezone"] = {
-            "sec": timezone_sec,
-            "UTC": f"UTC{z}{int(timezone_sec) / 3600:.0f}"
+            "offset_sec": timezone_sec,
+            "UTC": f"UTC{'+' if timezone_sec > 0 else ''}{timezone_sec / 3600:.0f}"
         }
     else:
-        weather_info["timezone"] = {"sec": None, "UTC": None}
+        weather_info["timezone"] = {"offset_sec": None, "UTC": None}
+
 
     return weather_info
 
 
-def render_data(data_forkast):
+
+
+
+def return_city_weather_data(lat, lon):
+    forkast_data = get_data(lat, lon)
+
     weather_data = {
       "clouds": [],
       "time": [],
@@ -82,63 +74,54 @@ def render_data(data_forkast):
       "wind_direction": []
     }
 
-    for i in data_forkast["list"]:
-        weather_data["clouds"].append(i.get("clouds", {}).get("all", None))
-        weather_data["time"].append(i.get("dt_txt", None))
-        weather_data["atmospheric_pressure_at_ground_level"].append(i.get("main", {}).get("grnd_level", None))
-        weather_data["atmospheric_pressure_at_sea_level"].append(i.get("main", {}).get("pressure", None))
-        weather_data["sea_level_pressure"].append(i.get("main", {}).get("sea_level", None))
-        weather_data["humidity"].append(i.get("main", {}).get("humidity", None))
-        weather_data["temperature"].append(i.get("main", {}).get("temp", None))
-        weather_data["min_temperature"].append(i.get("main", {}).get("temp_min", None))
-        weather_data["max_temperature"].append(i.get("main", {}).get("temp_max", None))
-        weather_data["feels_like"].append(i.get("main", {}).get("feels_like", None))
-        weather_data["probability_of_precipitation"].append(i.get("pop", None))
-        weather_data["visibility"].append(i.get("visibility", None))
-        weather_data["wind_gust_speed"].append(i.get("wind", {}).get("gust", None))
-        weather_data["wind_speed"].append(i.get("wind", {}).get("speed", None))
-        weather_data["wind_direction"].append(i.get("wind", {}).get("deg", None))
+    for i in forkast_data["list"]:
+        weather_data["clouds"].append(i.get("clouds", {}).get("all"))
+        weather_data["time"].append(i.get("dt_txt"))
+        weather_data["atmospheric_pressure_at_ground_level"].append(i.get("main", {}).get("grnd_level"))
+        weather_data["atmospheric_pressure_at_sea_level"].append(i.get("main", {}).get("pressure"))
+        weather_data["sea_level_pressure"].append(i.get("main", {}).get("sea_level",))
+        weather_data["humidity"].append(i.get("main", {}).get("humidity"))
+        weather_data["temperature"].append(i.get("main", {}).get("temp"))
+        weather_data["min_temperature"].append(i.get("main", {}).get("temp_min"))
+        weather_data["max_temperature"].append(i.get("main", {}).get("temp_max"))
+        weather_data["feels_like"].append(i.get("main", {}).get("feels_like"))
+        weather_data["probability_of_precipitation"].append(i.get("pop"))
+        weather_data["visibility"].append(i.get("visibility"))
+        weather_data["wind_gust_speed"].append(i.get("wind", {}).get("gust"))
+        weather_data["wind_speed"].append(i.get("wind", {}).get("speed"))
+        weather_data["wind_direction"].append(i.get("wind", {}).get("deg"))
 
     return weather_data
 
 
 
-def print_weather_info(weather_info):
-    result = "\n\n" + "-" * 35 + "\n"
-    
+
+
+def return_weather_info_string(lat, lon):
+    weather_info = return_city_info(lat, lon)
+
+    string =  "\n" + "* CITY INFO *" + "\n" + "-" * 35 + "\n"
     for key, value in weather_info.items():
         if isinstance(value, dict):
-            result += f"{key}:\n"
+            string += f"{key}:\n"
             for sub_key, sub_value in value.items():
-                result += f"  | {sub_key:5}:  {sub_value}\n"
+                string += f"  - {sub_key:11}:  {sub_value}\n"
         else:
-            result += f"{key:10}:  {value}\n"
-            
-        result += "-" * 35 + "\n"
+            string += f"{key:10}:  {value}\n"   
+        string += "-" * 35 + "\n"
     
-    result += "\n\n"
-    return result
+    return string
 
 
 
-def print_weather_data(weather_data):
-    result = "\n\n" + "-" * 120 + "\n"
+
+
+def return_weather_data_string(lat, lon):
+    weather_data = return_city_weather_data(lat, lon)
+
+    string = "\n" + "* WEATHER DATA *" + "\n" + "-" * 120 + "\n"
     for key, value in weather_data.items():
-        result += f"\n{key}:   {value}\n\n"
-        result += "-" * 120 + "\n"
-    result += "\n"
-    return result
+        string += f"\n{key}:   {value}\n"
+        string += "\n" + "-" * 120 + "\n"
 
-   
-   
-def return_weather_data(lat, lon):
-    data_forkast = get_data(lat, lon)
-    
-    data_dict = {}
-
-    data_dict["weather_info"] = render_info(data_forkast)
-    data_dict["weather_data"] = render_data(data_forkast)
-    data_dict["print_weather_info"] = print_weather_info(data_dict["weather_info"])
-    data_dict["print_weather_data"] = print_weather_data(data_dict["weather_data"])
-
-    return data_dict
+    return string
--- test.py
+++
@@ -1,7 +0,0 @@
-from data_api import return_weather_data
-from direct_api import get_suggestion
-from pprint import pprint
-
-pprint(get_suggestion("london"))
-
-#print(return_weather_data(24, 54)["print_weather_data"])
